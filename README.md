e1df971d34b23c23b99161841e1944226b478d76
Chad Swenson <cswens@gmail.com>
2026-02-15 16:11:10 -0600

    Device query works

--- README.md
+++ README.md
@@ -1,1 +1,8 @@
 # orin-gpu
+
+
+
+## Resources
+
+### Balena Example Containers
+https://github.com/balena-io-examples/jetson-examples/
---
+++ run.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+set -e
+
+IMAGE_NAME="orin-gpu"
+IMAGE_TAG="latest"
+
+docker run --rm -it --privileged \
+    "${IMAGE_NAME}:${IMAGE_TAG}" \
+    /bin/bash
+
+# git clone https://github.com/NVIDIA/cuda-samples.git && cd cuda-samples && git checkout 7ce058b4796783b3b7ca8196c25d5f5b9c380ec4 && cd Samples/1_Utilities/deviceQuery && make && modprobe tegra_drm && ./deviceQuery
